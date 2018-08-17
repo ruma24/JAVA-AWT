94c481ae20f6a342a4a626c6958e70727eb8ae47
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-17 21:31:47 +0530

    Create filledrectangle.java

---
+++ filledrectangle.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.applet.*;
+public class Test extends Applet
+
+{
+public void paint(Graphics g)
+{
+
+
+g.setColor(Color.orange);
+g.fillRect(55,20,120,30);
+
+g.setColor(Color.red);
+g.fillRect(55,50,120,30);
+
+g.setColor(Color.green);
+g.fillRect(55,80,120,30);
+
+
+}
+}
